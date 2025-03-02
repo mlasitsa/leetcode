15c1faee5a15a0895554f1288fcc0012cb0df115
Max <mlasitsa@css.edu>
2025-03-02 17:28:53 -0600

    commit

--- 1.Easy/704.BinarySearch.py
+++ 1.Easy/704.BinarySearch.py
@@ -26,7 +26,7 @@ class Solution:
         Observations:
         - The array is **sorted**, which suggests **binary search**
         - Binary search is an efficient way to search in a sorted array (O(log N) time complexity).
-        - The problem does not require modification of `nums`, so an iterative or recursive binary search will work.
+        - The problem does not require modification of `nums` so an iterative or recursive binary search will work.
 
         Approach:
         - Use **binary search**:
