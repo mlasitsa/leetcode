90e209ce7729594694e9516f756592369527f831
Max Lasitsa <mlasitsa@css.edu>
2025-09-21 20:47:24 -0500

    added new solution

--- 1.Easy/9.PalindromeNumber.py
+++ 1.Easy/9.PalindromeNumber.py
@@ -18,6 +18,31 @@ class Solution:
                 left += 1
                 right -= 1
         return True
+
+
+# Another optimal solution (without converting into a string)
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+
+        if x < 0:
+            return False
+        
+        div = 1
+        while x >= 10 * div:
+            div *= 10
+        
+        while x:
+            right = x % 10
+            left = x // div
+
+            if left != right:
+                return False
+            
+            x = (x % div) // 10
+            div =  div / 100
+        return True
+        
                 
             
         
